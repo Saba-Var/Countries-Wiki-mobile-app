2360d9267f294f929f50fc2617ff684aaf0e186b
Saba-Var <vartasashvili94@gmail.com>
2023-09-14 14:20:11 +0400

    feat: build country info card view with react-native-paper

--- components/home/CountryInfoCard/CountryInfoCard.tsx
+++ components/home/CountryInfoCard/CountryInfoCard.tsx
@@ -1,11 +1,43 @@
+import { Avatar, Button, Card } from 'react-native-paper'
 import { CountryInfoCardProps } from './types'
-import { View, Text } from 'react-native'
+
+const LeftContent = (props) => <Avatar.Icon {...props} icon='folder' />
 
 const CountryInfoCard: React.FC<CountryInfoCardProps> = ({ countryInfo }) => {
   return (
-    <View>
-      <Text>{countryInfo.name.common} </Text>
-    </View>
+    <Card
+      style={{
+        marginTop: 25,
+        width: '90%',
+        alignSelf: 'center',
+      }}
+      mode='elevated'
+    >
+      <Card.Title
+        title={countryInfo.name.common}
+        subtitle={countryInfo.region}
+      />
+
+      <Card.Cover
+        source={{ uri: countryInfo.flags.png }}
+        style={{
+          backgroundColor: 'transparent',
+          paddingHorizontal: 12,
+        }}
+      />
+
+      <Card.Actions>
+        <Button
+          style={{
+            marginTop: 10,
+          }}
+          mode='contained-tonal'
+          onPress={() => console.log('Pressed')}
+        >
+          Read More
+        </Button>
+      </Card.Actions>
+    </Card>
   )
 }
 
